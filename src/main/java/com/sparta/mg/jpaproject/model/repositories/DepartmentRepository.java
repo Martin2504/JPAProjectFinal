9db84e6c12b76d5e1ade21bdf2a0b2f0542654ea
MariaPanahi <mahmadpanahi@spartaglobal.com>
2023-03-01 16:17:57 +0000

    Services added

--- src/main/java/com/sparta/mg/jpaproject/model/repositories/DepartmentRepository.java
+++ src/main/java/com/sparta/mg/jpaproject/model/repositories/DepartmentRepository.java
@@ -2,6 +2,7 @@ package com.sparta.mg.jpaproject.model.repositories;
 
 import com.sparta.mg.jpaproject.model.entities.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DepartmentRepository extends JpaRepository<Department, String> {
 }
---
+++ src/main/java/com/sparta/mg/jpaproject/services/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.sparta.mg.jpaproject.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+   // Martin
+}
