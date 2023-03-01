e5a58e079e2e339dae9aee8913af4ec8e003b4ef
sp01186 <58943663+sp01186@users.noreply.github.com>
2023-03-01 17:57:58 +0000

    Done the majority of the method, just need to do tests and exceptions

--- src/main/java/com/sparta/mg/jpaproject/model/repositories/DeptEmpRepository.java
+++ src/main/java/com/sparta/mg/jpaproject/model/repositories/DeptEmpRepository.java
@@ -3,6 +3,20 @@ package com.sparta.mg.jpaproject.model.repositories;
 import com.sparta.mg.jpaproject.model.entities.DeptEmp;
 import com.sparta.mg.jpaproject.model.entities.DeptEmpId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 public interface DeptEmpRepository extends JpaRepository<DeptEmp, DeptEmpId> {
+
+    @Query(value = "SELECT de FROM DeptEmp de WHERE de.id.deptNo = :deptNo AND de.fromDate < :beforeDate AND de.toDate > :endDate")
+    List<DeptEmp> getDeptEmpsByDeptNo(@Param("deptNo") String deptNo, LocalDate beforeDate, LocalDate endDate);
+
+    @Query(value = "SELECT Count(de) FROM DeptEmp de WHERE de.id.deptNo = :deptNo AND de.fromDate < :beforeDate AND de.toDate > :endDate")
+    int getNoOfEmpsByDeptNoWithinTimePeriod(@Param("deptNo") String deptNo, LocalDate beforeDate, LocalDate endDate);
+
+
 }
--- src/main/java/com/sparta/mg/jpaproject/services/StaffService.java
+++ src/main/java/com/sparta/mg/jpaproject/services/StaffService.java
@@ -1,12 +1,15 @@
 package com.sparta.mg.jpaproject.services;
 
+import com.sparta.mg.jpaproject.model.entities.Department;
 import com.sparta.mg.jpaproject.model.repositories.DepartmentRepository;
 import com.sparta.mg.jpaproject.model.repositories.DeptEmpRepository;
 import com.sparta.mg.jpaproject.model.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -31,20 +34,20 @@ public class StaffService {
         this.departmentRepository = departmentRepository;
     }
 
-    public Map<String, Integer> getDeptAndNoOfEmpGivenDeptIdAndTimePeriod(int dept_id, Date start_Date, Date end_Date) {
-        return null;
+    public Map<String, Integer> getDeptAndNoOfEmpGivenTimePeriod(LocalDate start_Date, LocalDate end_Date) {
+        Map<String, Integer> departmentAndNumbers = new HashMap<>();
+        List<Department> departments = departmentRepository.findAll();
+        for (Department department: departments) {
+            String keyValue = department.getDeptName();
+            int totalNumber = 0;
+            totalNumber = deptEmpRepository.getNoOfEmpsByDeptNoWithinTimePeriod(department.getId(), start_Date, end_Date);
+            departmentAndNumbers.put(department.getDeptName(), totalNumber);
+        }
+        return departmentAndNumbers;
     }
 
-    public Map<String, Integer> getDeptAndNoOfEmpGivenDeptNameAndTimePeriod(String dept_Name, Date start_Date, Date end_Date) {
-        return null;
-    }
-
-    public Map<String, Integer> getDeptAndNoOfEmpGivenDeptIdAndYears(int dept_id, int start_Year, int end_Year) {
-        return null;
-    }
-
-    public Map<String, Integer> getDeptAndNoOfEmpGivenDeptNameAndYears(String dept_Name, int start_Year, int end_Year) {
-        return null;
+    public Map<String, Integer> getDeptAndNoOfEmpGivenYears(int start_Year, int end_Year) {
+        return getDeptAndNoOfEmpGivenTimePeriod(LocalDate.of(start_Year, 1, 1), LocalDate.of(end_Year, 1, 1));
     }
 
 
