bcda98c34016d840a83d7a936babe8007c74b612
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-03-01 17:26:37 +0000

    As a User I would like to be able to see all users when with a specific last name

--- src/main/java/com/sparta/mg/jpaproject/services/EmployeeService.java
+++ src/main/java/com/sparta/mg/jpaproject/services/EmployeeService.java
@@ -1,8 +1,24 @@
 package com.sparta.mg.jpaproject.services;
 
+import com.sparta.mg.jpaproject.model.entities.Employee;
+import com.sparta.mg.jpaproject.model.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
-   // Martin
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> findEmployeesByLastName(String lastName) {
+        return employeeRepository.findEmployeeGivenLastName(lastName);
+    }
+
 }
