d785023cde2020fa58cfd90e083f0213e2b3b59f
sp01186 <58943663+sp01186@users.noreply.github.com>
2023-03-02 16:44:41 +0000

    Fixed the main and a couple of the test cases

--- src/main/java/com/sparta/mg/jpaproject/JpaProjectApplication.java
+++ src/main/java/com/sparta/mg/jpaproject/JpaProjectApplication.java
@@ -23,36 +23,11 @@ import java.time.LocalDate;
 public class JpaProjectApplication {
     private Logger logger = LoggerFactory.getLogger(JpaProjectApplication.class);
 
-    Logger logger = LoggerFactory.getLogger(JpaProjectApplication.class);
-
     public static void main(String[] args) {
 
         SpringApplication.run(JpaProjectApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner runner (DeptEmpRepository deptEmpRepository){
-        return args ->{
-            for (Employee employee : deptEmpRepository.findEmployeeByDeptEmp("d006",LocalDate.of(1986, 2, 23))){
-                logger.info(employee.toString());
-            }
-        };
-    }
-
-
-/*    @Bean
-    public CommandLineRunner runner(SalaryService salaryService) {
-        return args -> salaryService.getDeptSalaryAvg("d005");
-    }*/
-
-    @Bean
-    public CommandLineRunner runner2(SalaryRepository salaryRepository) {
-        return args -> {
-            for(int s: salaryRepository.getSalariesByDept("d005")){
-               logger.info(String.valueOf(s));
-            }
-        };
-    }
 
 
 }
