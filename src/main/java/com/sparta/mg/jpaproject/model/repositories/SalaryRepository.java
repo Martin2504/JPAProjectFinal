c62abe5b7903220a7f2542473fd9533f06c84ca1
sp01186 <58943663+sp01186@users.noreply.github.com>
2023-03-02 16:12:39 +0000

    fixed conflict

--- src/main/java/com/sparta/mg/jpaproject/model/repositories/SalaryRepository.java
+++ src/main/java/com/sparta/mg/jpaproject/model/repositories/SalaryRepository.java
@@ -5,6 +5,7 @@ import com.sparta.mg.jpaproject.model.entities.Salary;
 import com.sparta.mg.jpaproject.model.entities.SalaryId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.time.LocalDate;
