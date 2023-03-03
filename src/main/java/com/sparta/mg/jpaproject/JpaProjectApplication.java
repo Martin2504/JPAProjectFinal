2609b641b139d5d1b23771db5855730426c9c474
HMalik <hmalik-@hotmail.com>
2023-03-03 16:09:20 +0000

    implemented Salaryrep an Service for avgSalary for a title and Year

--- src/main/java/com/sparta/mg/jpaproject/JpaProjectApplication.java
+++ src/main/java/com/sparta/mg/jpaproject/JpaProjectApplication.java
@@ -1,32 +1,16 @@
 package com.sparta.mg.jpaproject;
 
-
-import com.sparta.mg.jpaproject.model.repositories.SalaryRepository;
-import com.sparta.mg.jpaproject.services.EmployeeService;
-import com.sparta.mg.jpaproject.services.SalaryService;
-
-import com.sparta.mg.jpaproject.model.entities.Employee;
-import com.sparta.mg.jpaproject.model.repositories.DeptEmpRepository;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-
-
-import java.time.LocalDate;
-
 
 @SpringBootApplication
 public class JpaProjectApplication {
-    private Logger logger = LoggerFactory.getLogger(JpaProjectApplication.class);
 
     public static void main(String[] args) {
 
         SpringApplication.run(JpaProjectApplication.class, args);
     }
+
+
 
 }
