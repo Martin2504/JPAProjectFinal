9fd083a469ece9e1846ad68b6bc308b5937dd4cc
sp01186 <58943663+sp01186@users.noreply.github.com>
2023-03-08 14:56:07 +0000

    Made the basic departmentController and added a generator for it

--- src/main/java/com/sparta/mg/jpaproject/controllers/departmentcontrollers/DepartmentController.java
+++ src/main/java/com/sparta/mg/jpaproject/controllers/departmentcontrollers/DepartmentController.java
@@ -1,7 +1,60 @@
 package com.sparta.mg.jpaproject.controllers.departmentcontrollers;
 
+import com.sparta.mg.jpaproject.model.entities.Department;
+import com.sparta.mg.jpaproject.model.repositories.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class DepartmentController {
     //Samir
+
+    private final DepartmentRepository departmentRepository;
+
+    @Autowired
+    public DepartmentController(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    @PostMapping(value = "/department")
+    public String createNewDepartment(@RequestParam String departmentName) {
+        Department department = new Department();
+        department.setDeptName(departmentName);
+
+        departmentRepository.save(department);
+        return "Department " + departmentName + " successfully added";
+    }
+
+    @GetMapping("/departments")
+    public List<Department> getAllDepartments() {
+        return departmentRepository.findAll();
+    }
+
+    @PatchMapping("/department/{deptId}")
+    public String updateDepartmentNameWithId(@PathVariable String deptId, @RequestParam String deptName) {
+        Optional<Department> department = departmentRepository.findById(deptId);
+        if (department.isPresent()) {
+            departmentRepository.updateDepartmentNameById(deptName, deptId);
+            return "Successfully updated";
+        } else {
+            return "Unsuccessfully updated";
+        }
+    }
+
+    @DeleteMapping("/department/{deptId}")
+    public String deleteDepartmentWithId(@PathVariable String deptId) {
+        Optional<Department> department = departmentRepository.findById(deptId);
+
+        if (department.isPresent()) {
+            departmentRepository.deleteById(deptId);
+            return "Department successfully removed";
+        } else {
+            return "Department doesn't exist";
+        }
+    }
+
 
 }
