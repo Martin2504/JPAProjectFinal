195faf2e4c6ca4eb6750635fce0be556fb86ce10
sp01186 <58943663+sp01186@users.noreply.github.com>
2023-03-11 18:11:54 +0000

    done some departmentcontroller stuff

--- src/test/java/com/sparta/mg/jpaproject/controllers/departmentcontrollers/DepartmentControllerTest.java
+++ src/test/java/com/sparta/mg/jpaproject/controllers/departmentcontrollers/DepartmentControllerTest.java
@@ -1,7 +1,39 @@
 package com.sparta.mg.jpaproject.controllers.departmentcontrollers;
 
+import com.sparta.mg.jpaproject.model.repositories.DepartmentRepository;
+import com.sparta.mg.jpaproject.services.ApiKeyService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class DepartmentControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Mock
+    private DepartmentRepository departmentRepository;
+
+    @Mock
+    private ApiKeyService apiKeyService;
+
+    @InjectMocks
+    private DepartmentController departmentController;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(departmentController).build();
+    }
+
+
 
 }
