8212f5ed5f53546d3cc89211eed8611ad4e9ad57
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-03-16 13:47:18 +0000

    implemented Spring Security

---
+++ src/main/java/com/sparta/mg/jpaproject/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.sparta.mg.jpaproject.config;
+
+import com.sparta.mg.jpaproject.services.JpaUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JpaUserDetailsService jpaUserDetailsService;
+
+    public SecurityConfig(JpaUserDetailsService jpaUserDetailsService) {
+        this.jpaUserDetailsService = jpaUserDetailsService;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .userDetailsService(jpaUserDetailsService)
+                .headers(headers -> headers.frameOptions().sameOrigin())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        //return NoOpPasswordEncoder.getInstance(); //Bad!!!!!
+        return new BCryptPasswordEncoder();
+    }
+}
+
