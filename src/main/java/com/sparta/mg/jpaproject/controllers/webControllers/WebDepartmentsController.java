a5ecf13438875102930d6d249b424c82d72b11b5
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-03-16 13:47:55 +0000

    Added access rights to endpoints in DepartmentsController and SalariesController

--- src/main/java/com/sparta/mg/jpaproject/controllers/webControllers/WebDepartmentsController.java
+++ src/main/java/com/sparta/mg/jpaproject/controllers/webControllers/WebDepartmentsController.java
@@ -2,6 +2,7 @@ package com.sparta.mg.jpaproject.controllers.webControllers;
 
 import com.sparta.mg.jpaproject.model.entities.Department;
 import com.sparta.mg.jpaproject.model.repositories.DepartmentRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,47 +19,54 @@ public class WebDepartmentsController {
     }
 
     // Create a new Department
+    @PreAuthorize("hasRole('ROLE_UPDATE')")
     @GetMapping("/createDepartment")
     public String createDepartment(Model model) {
         Department department = new Department();
         model.addAttribute("departmentToCreate", department);
-        return "departments/department-create-form";
+        return "DepartmentsSubPages/department-create-form";
     }
 
     // Receives Department object to create from a form.
+    @PreAuthorize("hasRole('ROLE_UPDATE')")
     @PostMapping("/departmentReturn")
     public String departmentReturn(@ModelAttribute("departmentToCreate") Department department) {
         departmentRepository.save(department);
-        return "departments/success";
+        return "DepartmentsSubPages/success";
     }
 
+
     // View all Departments
+    @PreAuthorize("hasRole('ROLE_BASIC')")
     @GetMapping("/allDepartments")
     public String getAllDepartments(Model model) {
         model.addAttribute("departments", departmentRepository.findAll());
-        return "departments/all-departments";
+        return "DepartmentsSubPages/all-departments";
     }
 
     // Requesting to edit a department
+    @PreAuthorize("hasRole('ROLE_UPDATE')")
     @GetMapping("/department/edit/{id}")
     public String getDepartmentToEdit(@PathVariable String id, Model model) {
         Department department = departmentRepository.findById(id).orElse(null);
         model.addAttribute("departmentToEdit", department);
-        return "departments/department-edit-form";
+        return "DepartmentsSubPages/department-edit-form";
     }
 
     // Receives Department Object to edit from a form.
+    @PreAuthorize("hasRole('ROLE_UPDATE')")
     @PostMapping("/updateDepartment")
     public String updateDepartment(@ModelAttribute("departmentToEdit") Department editedDepartment) {
         departmentRepository.saveAndFlush(editedDepartment);
-        return "departments/success";
+        return "DepartmentsSubPages/success";
     }
 
     // Delete a department
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/department/delete/{id}")
     public String deleteDepartment(@PathVariable String id) {
         departmentRepository.deleteById(id);
-        return "departments/success";
+        return "DepartmentsSubPages/success";
     }
 
 
