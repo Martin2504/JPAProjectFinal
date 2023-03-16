334412dffd73e85b093834518ac443fe0dcef7cc
sp01186 <58943663+sp01186@users.noreply.github.com>
2023-03-16 15:56:11 +0000

    Completed the update portion Department Manager

--- src/main/java/com/sparta/mg/jpaproject/controllers/webControllers/WebDept_ManagerController.java
+++ src/main/java/com/sparta/mg/jpaproject/controllers/webControllers/WebDept_ManagerController.java
@@ -9,9 +9,8 @@ import com.sparta.mg.jpaproject.model.repositories.DeptManagerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -58,7 +57,7 @@ public class WebDept_ManagerController {
 
     //Update
 
-    @GetMapping("/updateDeptManager")
+    @GetMapping("/editDeptManager")
     public String updateDeptManager(Model model, @RequestParam String deptId, @RequestParam Integer emp_No) {
         DeptManagerId deptManagerId = new DeptManagerId();
         deptManagerId.setEmpNo(emp_No);
@@ -66,6 +65,13 @@ public class WebDept_ManagerController {
         DeptManager deptManager = deptManagerRepository.findById(deptManagerId).orElse(null);
         model.addAttribute("deptManagerToEdit", deptManager);
         return "dept_manager_files/department-manager-edit-form";
+    }
+
+    @PostMapping("/update")
+    public String updateSalary(@ModelAttribute("deptManagerToEdit") DeptManager deptManager, RedirectAttributes redirectAttributes){
+        deptManagerRepository.saveAndFlush(deptManager);
+        redirectAttributes.addFlashAttribute("status", "Salary successfully updated");
+        return "redirect:/dept-manager/getDeptManagers?deptId=" + deptManager.getId().getDeptNo();
     }
 
     //Delete
