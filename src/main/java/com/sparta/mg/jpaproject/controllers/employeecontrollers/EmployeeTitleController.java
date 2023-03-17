e370eeb1576a52e083e9f12585c9f2ca8aa0adeb
HMalik <hmalik-@hotmail.com>
2023-03-17 14:24:59 +0000

    completed webtitleController

--- src/main/java/com/sparta/mg/jpaproject/controllers/employeecontrollers/EmployeeTitleController.java
+++ src/main/java/com/sparta/mg/jpaproject/controllers/employeecontrollers/EmployeeTitleController.java
@@ -5,14 +5,16 @@ import com.sparta.mg.jpaproject.model.entities.Title;
 import com.sparta.mg.jpaproject.model.entities.TitleId;
 import com.sparta.mg.jpaproject.model.repositories.EmployeeRepository;
 import com.sparta.mg.jpaproject.model.repositories.TitleRepository;
+import com.sparta.mg.jpaproject.services.ApiKeyService;
+import com.sparta.mg.jpaproject.tools.CRUD;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,17 +25,24 @@ public class EmployeeTitleController {
     private final TitleRepository titleRepository;
     private final EmployeeRepository employeeRepository;
     private final ObjectMapper mapper;
+    private final ApiKeyService apiKeyService;
 
     @Autowired
-    public EmployeeTitleController(TitleRepository titleRepository, ObjectMapper mapper, EmployeeRepository employeeRepository) {
+    public EmployeeTitleController(TitleRepository titleRepository, ObjectMapper mapper, EmployeeRepository employeeRepository, ApiKeyService apiKeyService) {
         this.titleRepository = titleRepository;
         this.mapper = mapper;
         this.employeeRepository = employeeRepository;
+        this.apiKeyService = apiKeyService;
     }
 
-    @GetMapping(value = "/employee/title/{id}")
-    public ResponseEntity<String> getListOfTitlesById(@PathVariable Integer id) {
+  /*  @GetMapping(value = "/employee/title/{id}")
+    public ResponseEntity<String> getListOfTitlesById(@PathVariable Integer id , @RequestHeader("x-api-key") String apiKey) {
+        if (!apiKeyService.validateUser(apiKey, CRUD.READ)) {
+            return apiKeyService.getInvalidApiKeyResponse();
+        }
         Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("content-type", "application/json");
         if (optionalEmployee.isPresent()) {
 
             List<Title> titles = titleRepository.findTitleByEmpNo(optionalEmployee.get());
@@ -41,9 +50,54 @@ public class EmployeeTitleController {
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Employee not found");
         }
+    } */
+
+   /* @GetMapping(value = "/employee/title/{id}")
+    public ResponseEntity<String> getListOfTitlesById(@PathVariable Integer id, @RequestHeader("x-api-key") String apiKey) {
+        try {
+            if (!apiKeyService.validateUser(apiKey, CRUD.READ)) {
+                return apiKeyService.getInvalidApiKeyResponse();
+            }
+            Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("content-type", "application/json");
+            if (optionalEmployee.isPresent()) {
+                List<Title> titles = titleRepository.findTitleByEmpNo(optionalEmployee.get());
+                return ResponseEntity.ok(titles.toString());
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Employee not found");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred: " + e.getMessage());
+        }
+    } */
+
+    @GetMapping(value = "/employee/title/{id}")
+    public ResponseEntity<String> getListOfTitlesById(@PathVariable Integer id, @RequestHeader("x-api-key") String apiKey) {
+        try {
+            if (!apiKeyService.validateUser(apiKey, CRUD.READ)) {
+                return apiKeyService.getInvalidApiKeyResponse();
+            }
+            Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+            if (optionalEmployee.isPresent()) {
+                List<Title> titles = titleRepository.findTitleByEmpNo(optionalEmployee.get());
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                return ResponseEntity.ok()
+                        .headers(httpHeaders)
+                        .body(titles.toString());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error occurred: " + e.getMessage());
+        }
     }
 
-    @PatchMapping(value = "/employee/{id}/{title}/{fromDate}")
+
+
+   /* @PatchMapping(value = "/employee/{id}/{title}/{fromDate}")
     public ResponseEntity<String> updateTitleById(
             @PathVariable("id") Integer id,
             @PathVariable("title") String title,
@@ -59,9 +113,40 @@ public class EmployeeTitleController {
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ToDate not found");
         }
+    } */
+
+
+    @PatchMapping(value = "/employee/{id}/{title}/{fromDate}")
+    public ResponseEntity<String> updateTitleById(
+            @PathVariable("id") Integer id,
+            @PathVariable("title") String title,
+            @PathVariable("fromDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fromDate,
+            @RequestParam("toDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate toDate,
+            @RequestHeader("x-api-key") String apiKey) {
+
+        try {
+            if (!apiKeyService.validateUser(apiKey, CRUD.UPDATE)) {
+                return apiKeyService.getInvalidApiKeyResponse();
+            }
+
+            Optional<Title> existingTitle = titleRepository.findTitleByEmpFromDateTitle(id, fromDate, title);
+
+            if (existingTitle.isPresent()) {
+                existingTitle.get().setToDate(toDate);
+                titleRepository.save(existingTitle.get());
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                return ResponseEntity.ok().headers(httpHeaders).body("ToDate Updated");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ToDate not found");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred: " + e.getMessage());
+        }
     }
 
-    @PostMapping(value = "/employee/{id}/title")
+
+  /*  @PostMapping(value = "/employee/{id}/title")
     public ResponseEntity<String> createNewTitle(
             @PathVariable("id") Integer id,
             @RequestParam("title") String title,
@@ -87,9 +172,43 @@ public class EmployeeTitleController {
                     new HttpHeaders(),
                     HttpStatus.NOT_FOUND);
         }
+    } */
+
+    @PostMapping(value = "/employee/{id}/title")
+    public ResponseEntity<String> createNewTitle(
+            @PathVariable("id") Integer id,
+            @RequestParam("title") String title,
+            @RequestParam("fromDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fromDate,
+            @RequestParam("toDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate toDate,
+            @RequestHeader("x-api-key") String apiKey) {
+
+        if (!apiKeyService.validateUser(apiKey, CRUD.CREATE)) {
+            return apiKeyService.getInvalidApiKeyResponse();
+        }
+
+        Optional<Employee> emp1 = employeeRepository.findById(id);
+        if (emp1.isPresent()) {
+            Title title1 = new Title();
+            title1.setToDate(toDate);
+            TitleId titleId = new TitleId();
+            titleId.setTitle(title);
+            titleId.setEmpNo(id);
+            titleId.setFromDate(fromDate);
+            title1.setId(titleId);
+            title1.setEmpNo(emp1.get());
+            System.out.println(title1);
+            titleRepository.save(title1);
+            return ResponseEntity.ok("New Title created");
+        } else {
+            return new ResponseEntity<>(
+                    "Employee With " + id + " does not exist",
+                    new HttpHeaders(),
+                    HttpStatus.NOT_FOUND);
+        }
     }
 
-    @DeleteMapping(value = "/employee/{id}/title")
+
+   /* @DeleteMapping(value = "/employee/{id}/title")
     public ResponseEntity<String> deleteTitle(
             @PathVariable("id") Integer id,
             @RequestParam("title") String title,
@@ -114,5 +233,45 @@ public class EmployeeTitleController {
                     new HttpHeaders(),
                     HttpStatus.NOT_FOUND);
         }
+    } */
+
+    @DeleteMapping(value = "/employee/{id}/title")
+    public ResponseEntity<String> deleteTitle(
+            @PathVariable("id") Integer id,
+            @RequestParam("title") String title,
+            @RequestParam("fromDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fromDate,
+            @RequestParam("toDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate toDate,
+            @RequestHeader("x-api-key") String apiKey) {
+
+        if (apiKey == null || apiKey.isEmpty()) {
+            return new ResponseEntity<>(
+                    "API Key not found",
+                    new HttpHeaders(),
+                    HttpStatus.UNAUTHORIZED);
+        }
+
+        if (!apiKeyService.validateUser(apiKey, CRUD.DELETE)) {
+            return apiKeyService.getInvalidApiKeyResponse();
+        }
+
+        Optional<Employee> emp = employeeRepository.findById(id);
+        if (emp.isPresent()) {
+            Title title1 = new Title();
+            TitleId titleId = new TitleId();
+            title1.setToDate(toDate);
+            titleId.setTitle(title);
+            titleId.setEmpNo(id);
+            titleId.setFromDate(fromDate);
+            title1.setId(titleId);
+            title1.setEmpNo(emp.get());
+            titleRepository.delete(title1);
+            return ResponseEntity.ok("Title Deleted");
+        } else {
+            return new ResponseEntity<>(
+                    "Employee with ID " + id + " not found",
+                    new HttpHeaders(),
+                    HttpStatus.NOT_FOUND);
+        }
     }
+
 }
--- src/main/java/com/sparta/mg/jpaproject/controllers/webControllers/WebTitlesController.java
+++ src/main/java/com/sparta/mg/jpaproject/controllers/webControllers/WebTitlesController.java
@@ -1,7 +1,116 @@
 package com.sparta.mg.jpaproject.controllers.webControllers;
 
+import com.sparta.mg.jpaproject.model.entities.*;
+import com.sparta.mg.jpaproject.model.repositories.EmployeeRepository;
+import com.sparta.mg.jpaproject.model.repositories.TitleRepository;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Controller
 public class WebTitlesController {
+
+    private final TitleRepository titleRepository;
+    private final EmployeeRepository employeeRepository;
+
+    public WebTitlesController(TitleRepository titleRepository, EmployeeRepository employeeRepository) {
+        this.titleRepository = titleRepository;
+        this.employeeRepository = employeeRepository;
+    }
+
+
+
+    //Create
+    @PreAuthorize("hasRole('ROLE_UPDATE')")
+    @GetMapping("/createTitle")
+    public String createNewTitle(Model model) {
+
+        Title title = new Title();
+
+        model.addAttribute("titleToCreate", title);
+        return "TitlesPages/create-job-title";
+    }
+
+    @PreAuthorize("hasRole('ROLE_UPDATE')")
+    @PostMapping("/title/save")
+    public String save(@ModelAttribute("titleToCreate") Title titleToCreate,
+                       @RequestParam Integer empNo,
+                       Model model,
+                       RedirectAttributes redirectAttributes) {
+
+        titleRepository.save(titleToCreate);
+        Optional<Employee> employee = employeeRepository.findById(titleToCreate.getEmpNo().getId());
+        model.addAttribute("employee", employee);
+        redirectAttributes.addFlashAttribute("status", "New Title Created");
+//        return "redirect:/employee/titles?empNo=" + employee.get().getId();
+        return "titles-page";
+    }
+
+    // Read
+
+    @PreAuthorize("hasRole('ROLE_BASIC')")
+    @GetMapping("/title/search")
+    public String getTitleSearchPage() {
+        return "TitlesPages/searchTitlesPage";
+    }
+
+
+    @PreAuthorize("hasRole('ROLE_BASIC')")
+    @GetMapping("/viewTitles")
+    public String viewTitles(Model model, @RequestParam Integer empNo) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(empNo);
+        if(optionalEmployee.isPresent()) {
+            model.addAttribute("titlesToView", titleRepository.findTitleByEmpNo(optionalEmployee.get()));
+            model.addAttribute("employeeNo", optionalEmployee.get().getId());
+        }
+        return "TitlesPages/view-job-titles";
+    }
+
+    // Update
+
+    @PreAuthorize("hasRole('ROLE_UPDATE')")
+    @PostMapping("/title/edit")
+    public String getEditTitle(@RequestParam("empNo") Integer empNo,
+                               @RequestParam("title") String title,
+                               @RequestParam("fromDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fromDate,
+                               Model model) {
+
+        TitleId titleId = new TitleId();
+        titleId.setEmpNo(empNo);
+        titleId.setTitle(title);
+        titleId.setFromDate(fromDate);
+        Title title1 = titleRepository.findById(titleId).orElse(null);
+        model.addAttribute("titleToEdit", title1);
+        return "TitlesPages/update-job-title";
+    }
+
+    @PreAuthorize("hasRole('ROLE_UPDATE')")
+    @PostMapping("/title/update")
+    public String updateTitle(Title titleToEdit) {
+        titleRepository.saveAndFlush(titleToEdit);
+        return "titles-page";
+    }
+
+
+    // Delete
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping ("/title/delete")
+    public String deleteTitle(@RequestParam("empNo") Integer empNo,
+                              @RequestParam("title") String title,
+                              @RequestParam("fromDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fromDate ) {
+
+        TitleId titleId = new TitleId();
+        titleId.setEmpNo(Integer.valueOf(empNo));
+        titleId.setTitle(title);
+        titleId.setFromDate(fromDate);
+        titleRepository.deleteById(titleId);
+        return "titles-page";
+    }
+
 }
