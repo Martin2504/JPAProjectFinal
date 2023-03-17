85318977871bf0f2bd76701b36c6df528ed0ef54
sp01186 <58943663+sp01186@users.noreply.github.com>
2023-03-17 15:27:02 +0000

    completed the session implementation

--- src/main/java/com/sparta/mg/jpaproject/controllers/webControllers/WebEmployeesController.java
+++ src/main/java/com/sparta/mg/jpaproject/controllers/webControllers/WebEmployeesController.java
@@ -1,21 +1,21 @@
 package com.sparta.mg.jpaproject.controllers.webControllers;
 
-import com.sparta.mg.jpaproject.model.entities.Employee;
-import com.sparta.mg.jpaproject.model.repositories.DepartmentRepository;
-import com.sparta.mg.jpaproject.model.repositories.EmployeeRepository;
-import com.sparta.mg.jpaproject.model.repositories.TitleRepository;
+import com.sparta.mg.jpaproject.model.entities.*;
+import com.sparta.mg.jpaproject.model.repositories.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Controller
 @SessionAttributes({"employeeToCreateExtra", "deptId",
         "titleName", "titleInput", "salaryInput", "titleValue"})
+
 public class WebEmployeesController {
     private final EmployeeRepository employeeRepository;
 
@@ -23,15 +23,83 @@ public class WebEmployeesController {
 
     private final TitleRepository titleRepository;
 
-    public WebEmployeesController(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository, TitleRepository titleRepository) {
+    private final SalaryRepository salaryRepository;
+
+    private final DeptEmpRepository deptEmpRepository;
+
+    public WebEmployeesController(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository, TitleRepository titleRepository, SalaryRepository salaryRepository, DeptEmpRepository deptEmpRepository) {
         this.employeeRepository = employeeRepository;
         this.departmentRepository = departmentRepository;
         this.titleRepository = titleRepository;
+        this.salaryRepository = salaryRepository;
+        this.deptEmpRepository = deptEmpRepository;
     }
 
     @ModelAttribute("employeeToCreateExtra")
     public Employee employeeToCreateExtra() {
         return new Employee();
+    }
+
+    @ModelAttribute("deptId")
+    public String deptId() {
+        return "deptId value";
+    }
+
+    @ModelAttribute("titleName")
+    public String titleName() {
+        return "titleName value";
+    }
+
+    @ModelAttribute("titleInput")
+    public String titleInput() {
+        return "titleInput value";
+    }
+
+    @ModelAttribute("salaryInput")
+    public Integer salaryInput() {
+        return 0;
+    }
+
+    @ModelAttribute("titleValue")
+    public String titleValue() {
+        return "titleValue value";
+    }
+
+    private Title setTitle(Employee employee, String titleName) {
+        Title title1 = new Title();
+        TitleId titleId = new TitleId();
+        titleId.setTitle(titleName);
+        titleId.setEmpNo(employee.getId());
+        titleId.setFromDate(LocalDate.now());
+        title1.setToDate(LocalDate.of(9999, 01, 01));
+        title1.setEmpNo(employee);
+        title1.setId(titleId);
+        return title1;
+    }
+
+    private Salary setSalary(Employee employee, Integer salary) {
+        Salary salary1 = new Salary();
+        SalaryId salaryId = new SalaryId();
+        salaryId.setEmpNo(employee.getId());
+        salaryId.setFromDate(LocalDate.now());
+        salary1.setId(salaryId);
+        salary1.setSalary(salary);
+        salary1.setToDate(LocalDate.of(9999, 01,01));
+        salary1.setEmpNo(employee);
+        return salary1;
+    }
+
+    private DeptEmp setDeptEmp(Employee employee, String deptId) {
+        DeptEmp deptEmp = new DeptEmp();
+        DeptEmpId deptEmpId = new DeptEmpId();
+        deptEmpId.setDeptNo(deptId);
+        deptEmpId.setEmpNo(employee.getId());
+        deptEmp.setId(deptEmpId);
+        deptEmp.setDeptNo(departmentRepository.findById(deptId).get());
+        deptEmp.setFromDate(LocalDate.now());
+        deptEmp.setToDate(LocalDate.of(9999, 01, 01));
+        deptEmp.setEmpNo(employee);
+        return deptEmp;
     }
 
     @PreAuthorize("hasRole('ROLE_UPDATE')")
@@ -63,15 +131,16 @@ public class WebEmployeesController {
     }
 
     @PreAuthorize("hasRole('ROLE_UPDATE')")
-    @PostMapping("/employee/saveExtra")
+    @GetMapping("/employee/saveExtra")
     public String saveExtra(@ModelAttribute("employeeToCreateExtra") Employee employeeToCreate,
-                       @ModelAttribute("deptId") String deptId,
-                            @ModelAttribute("titleName") String titleName,
-                            @ModelAttribute("titleInput") String titleInput,
-                            @ModelAttribute("salaryInput") Integer salaryInput,
-                            Model model,
-                       RedirectAttributes redirectAttributes) {
+                       String deptId,
+                            String titleName,
+                            String titleInput,
+                            Integer salaryInput,
+                            Model model) {
         System.out.println(employeeToCreate);
+        model.addAttribute("deptId", deptId);
+        model.addAttribute("salaryInput", salaryInput);
         System.out.println(deptId);
         System.out.println(titleName);
         System.out.println(titleInput);
@@ -83,9 +152,30 @@ public class WebEmployeesController {
         }
 
         model.addAttribute("deptName", departmentRepository.findById(deptId).get().getDeptName());
-//        employeeRepository.save(employeeToCreate);
-//        redirectAttributes.addFlashAttribute("status", "Employee successfully created");
         return "/EmployeePages/create-employee-extra-confirmation";
+    }
+
+
+    @PreAuthorize("hasRole('ROLE_UPDATE')")
+    @PostMapping("/employee/confirmExtra")
+    public String saveExtras(RedirectAttributes redirectAttributes, Model model) {
+        Employee employee = (Employee) model.getAttribute("employeeToCreateExtra");
+        String deptId = (String) model.getAttribute("deptId");
+        String title = (String) model.getAttribute("titleValue");
+        Integer salary = (Integer) model.getAttribute("salaryInput");
+        employeeRepository.save(employee);
+        System.out.println(deptId);
+        System.out.println(title);
+        System.out.println(salary);
+        System.out.println(employee);
+        Salary salary1 = setSalary(employee, salary);
+        salaryRepository.save(salary1);
+        Title title1 = setTitle(employee, title);
+        titleRepository.save(title1);
+        DeptEmp deptEmp = setDeptEmp(employee, deptId);
+        deptEmpRepository.save(deptEmp);
+        redirectAttributes.addFlashAttribute("status", "Employee successfully created");
+        return "redirect:/employee?empNo=" + employee.getId();
     }
 
     @PreAuthorize("hasRole('ROLE_BASIC')")
